a0b34822a79107b9de1135e5b254f536cf822a68
Inolas <salonibuddhadeo@gmail.com>
2018-03-18 21:38:31 -0700

    Check In function

--- CreateMain.java
+++ CreateMain.java
@@ -1,10 +1,3 @@
-import java.io.*;
-import java.util.Scanner;
-import org.json.simple.parser.ParseException;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-
 /*
  * class name: CreateMain
  * Authors: Derek Baker
@@ -22,8 +15,15 @@ import org.json.simple.JSONArray;
  * Also added the Checkout function to create a local copy of a guven project version corresponding to the manifest file referred to by the user.
  * 
  */
+import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class CreateMain {    
+    static String a;
+    static String source_name="";
     
     public static void CreateSource(String[] args) throws IOException {
         String target;
@@ -33,102 +33,23 @@ public class CreateMain {
         new File(args[1]+"/"+args[0]).mkdir();
 
         //Create manifest file
-        String a = args[1]+"/mani_create.txt";
-		BufferedWriter manibw = new BufferedWriter(new FileWriter(a));	
-		manibw.write("create " + args[0] + " " + args[1]);
-		manibw.close();
+        a = args[1]+"/mani_create_.json";
+        BufferedWriter manibw = new BufferedWriter(new FileWriter(a));	//------------APPENDING ISSUES IN FILE
+	manibw.write("//CREATE " + args[0] + "/" + args[1]+"\n");
+	manibw.close();
 
         System.out.println("Repo created successfully.");
         target = args[1] + "/";
-        showFiles(files, args[0], target);                      
+        showFiles(files, args[0], target);                      //args[1] + "/" + 
         }
 
-    public static void showFiles(File[] files, String repo_name, String target) {
-        String parent, file_name, dir_name, repo_path, artID;
-        for (File file : files) {
-            repo_path = repo_name + "/";
-
-            if (file.isDirectory()) {                                   //Check if folder or not
-                dir_name = file.getName();                              //dir_name = name of the folder
-                System.out.println("Dir: " + dir_name);
-
-                repo_path += file.getName();                            //repository PATH name - repo_path
-                String newFolder = target+repo_path;
-                System.out.println( newFolder );                
-                new File(newFolder).mkdirs();                             
-                showFiles(file.listFiles(), repo_path, target);         //Calls same method again
-            }
-            else {
-                file_name = file.getName();
-                System.out.println("File: " + file_name);               //name of the file
-                /*String folderName = repo_path;*/
-                repo_path += file_name;
-                new File(target+repo_path).mkdir();
-//              String file_path = repo_path + file_name;               //file path = repository path + the folder created for file to save artifact files
-                System.out.println(repo_path+" repo_path");
-                try{
-                     artID = copyFile(repo_path,target);  
-                     createManifest.Manifest(repo_path, artID, target);                  
-                }
-                catch(Exception e){
-                    System.out.println(e+" Exception");
-                }
-            }
-        }
-    }
-    public static String copyFile(String file_name, String target) throws IOException{
-        
-        FileReader file = new FileReader(file_name);
-        BufferedReader br=new BufferedReader(file);
-
-        String repo_file, file_content="";
-        int i, checksum=0, counter=0;
-        repo_file = artID(file_name);
-        String fileDest = target+file_name;
-        BufferedWriter bw = new BufferedWriter(new FileWriter(fileDest+"/"+repo_file));
- 
-        while((i=br.read())!=-1){
-            file_content += (char)i;
-        }
-        bw.write(file_content);
-        
-        br.close();
-        bw.close();
-        file.close();
-        return repo_file;
-    }
-public static String artID(String file_name) throws IOException{
-        // String file_name = "hp.txt";
-
-        FileReader file = new FileReader(file_name);
-        File afile = new File(file_name);
-        BufferedReader br=new BufferedReader(file);
-        String art_file_name;
-        int i, checksum=0, counter=0;
-        char cha = '\0';
-        int weights[] = {1,7,11,17};
-
-        while((i=br.read())!=-1){
-            cha = (char)i;                                      //Converting string to char
-            checksum += (int) (char) i * weights[counter%4];    //checksum calculation - ascii of character * corresponding weight
-            counter++;                                          //counter for weights
-        }
-        checksum -= (int) cha * weights[(counter-1)%4];
-        checksum %= (Math.pow(2,31) - 1);
-        art_file_name = checksum+"-L"+afile.length()+".txt";    // artifactID - new file name
-        br.close();
-        file.close();
-
-        return art_file_name;
-    }
-//Function to checkout files
+    //Function to checkout files
     //Main.java passes three arguments - repofolder, manifest, targetfolder
     public static void Checkout(String[] args) throws IOException, ParseException {
 
         String src = args[0];
         String mani = args[1];
         String dest = args[2];
-
         int i;
 
         JSONParser parser = new JSONParser();
@@ -166,8 +87,7 @@ public static String artID(String file_name) throws IOException{
                 System.out.println("Creating file... "+tgtfolder+"/"+myFileName);
                 
             }
-            else 
-            {
+            else{
                 System.out.println("Creating directory: "+tgtfolder);
                 new File(tgtfolder).mkdirs();
             }
@@ -179,12 +99,148 @@ public static String artID(String file_name) throws IOException{
             FileOutputStream FO = new FileOutputStream(tgtfolder+"/"+myFileName);
             int b;
             //read content and write in another file
-            while((b=FI.read())!=-1)
-            { 
+            while((b=FI.read())!=-1){ 
                 FO.write(b);
             }
             System.out.println("File Copied...");
             FI.close();
     }
 }
+    //Function to checkin files
+    //Main.java passes two arguments - check in folder, target repo folder    
+   public static void Checkin(String[] args) throws IOException{
+       String src = args[0]+"/";                   //merge from path enterd by the user 
+       
+       String target = args[1]+"/source";                //merge to path, entered by the user
+       String cmd = "cin";
+       int i=1;
+        
+        File[] files = new File(src).listFiles();                   //list the files/dirs in the first argument
+
+        a = args[1]+"/mani_cin_1_.json";
+        if(new File(a).exists()){
+            i = createManifest.maniFileNo(args[1], cmd) + 1;
+            a = args[1]+"/mani_cin_"+(i)+"_.json";
+        }    
+        
+        BufferedWriter manibw = new BufferedWriter(new FileWriter(a));	//------------APPENDING ISSUES IN FILE
+        //Create manifest file
+        manibw.write("//CHECKIN " + args[0] +" "+ args[1]+"\n");
+        manibw.close();
+        
+        System.out.println("File Checked in successfully.");
+        
+        showFiles(files, args[0], target);                  //args[1] + "/" + 
+    }
+    
+    public static void showFiles(File[] files, String repo_, String target) {
+        String parent, file_name, dir_name, repo_path, artID, repo_name;
+
+        for (File file : files) {
+            repo_name = repo_+"/";
+            repo_path = target+"/";
+            
+            if (file.isDirectory()) {                                   //Check if folder or not
+                dir_name = file.getName();                              //dir_name = name of the folder
+                System.out.println("Dir: " + dir_name);
+
+                repo_path += file.getName();                            //repository PATH name - repo_path
+                repo_name += dir_name;
+                
+                String newFolder = repo_path;
+                System.out.println( newFolder+"new filesdfd" );
+                if(!new File(newFolder).exists()) {
+                        new File(newFolder).mkdirs();                             //ONLY CREATES A DIR IN THE REPO
+                }                             //ONLY CREATES A DIR IN THE REPO
+//                target+= 
+                showFiles(file.listFiles(), repo_name, repo_path);           //Calls same method again
+            }
+            else {
+                file_name = file.getName();
+                System.out.println("File: " + file_name);               //name of the file
+                /*String folderName = repo_path;*/
+                
+                repo_path += file_name;
+                repo_name += file_name;
+                if(!new File(repo_path).exists())    // if folder target+repo_path exists
+                    new File(repo_path).mkdir();
+                
+//              String file_path = repo_path + file_name;               //file path = repository path + the folder created for file to save artifact files
+                System.out.println(repo_path+" repo_path");
+                try{
+                     artID = copyFile(repo_name,repo_path);
+//                     if(System.out.print(artID != null)){
+                        System.out.println("-------0-0-0-0-0-0-"+artID+"-0-0-0-0-0-0-0--------"+a+repo_path);
+                        createManifest.Manifest(repo_path, artID, a);  // a manifest name track -_-
+  //                   }
+                }
+                catch(Exception e){
+                    System.out.println(e+" Exception!" + repo_path);
+                }
+            }
+        }
+    }
+
+    public static String copyFile(String file_name, String target) throws IOException{
+
+        FileReader file = new FileReader(file_name);
+        BufferedReader br=new BufferedReader(file);
+
+        String artId, file_content="";
+        int i, checksum=0, counter=0;
+        artId = artID(file_name);
+        String fileDest = target;
+
+        String inputName  = artId;
+        String workingDir = System.getProperty("user.dir");
+        // a File instance for the directory:
+        File workingDirFile = new File(workingDir);
+        // a File instance for a file in that directory:
+        File testfile = new File(workingDirFile, inputName);
+
+        if(!testfile.exists()){
+            //new File(fileDest).mkdir();
+            BufferedWriter bw=new BufferedWriter(new FileWriter(fileDest+"/"+artId));
+
+            while((i=br.read())!=-1){
+                file_content += (char)i;
+            }
+            bw.write(file_content);
+
+            br.close();
+            bw.close();
+            file.close();
+            return artId;
+        }
+        else{
+            System.out.print("null");
+            return null;
+        }
+    }
+public static String artID(String file_name) throws IOException{
+        // String file_name = "hp.txt";
+
+        FileReader file = new FileReader(file_name);
+        File afile = new File(file_name);
+        BufferedReader br=new BufferedReader(file);
+        String art_file_name;
+        int i, checksum=0, counter=0;
+        char cha = '\0';
+        int weights[] = {1,7,11,17};
+        // double file_size = file.length();
+
+        while((i=br.read())!=-1){
+            cha = (char)i;                                      //Converting string to char
+            checksum += (int) (char) i * weights[counter%4];    //checksum calculation - ascii of character * corresponding weight
+            counter++;                                          //counter for weights
+        }
+        checksum -= (int) cha * weights[(counter-1)%4];
+        checksum %= (Math.pow(2,31) - 1);
+        art_file_name = checksum+"-L"+afile.length()+".txt";    // artifactID - new file name
+
+        br.close();
+        file.close();
+
+        return art_file_name;
+    }
 }
