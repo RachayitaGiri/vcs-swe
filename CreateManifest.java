6a8334efb153ab11a45c3e5ca6b529b70ffddafd
Inolas <salonibuddhadeo@gmail.com>
2018-05-01 22:06:15 -0700

    Update CreateManifest.java

--- CreateManifest.java
+++ CreateManifest.java
@@ -21,34 +21,40 @@ import org.json.simple.parser.JSONParser;
  */
 
 
-public class CreateManifest {
-    public static void Manifest(String repo_path, String artID, String target) throws IOException {
+public class createManifest {
+    public static void Manifest(String ned_stark, String artID, String mani_path) throws IOException {
         try {
             Date date = new Date();
             SimpleDateFormat ft = new SimpleDateFormat ("yyyy:MM:dd hh:mm:ss");
+        
             /*
                 To record:
                     path name
                     artifact ID
-                    date and time
+              c      date and time
             */
             String timestamp;
-            String b = target;
-            JSONObject jsonMani = new JSONObject();
-            jsonMani.put("path", repo_path);
-            jsonMani.put("artID", artID);
-            jsonMani.put("Date", ft.format(date));
-
-            Writer  out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(b, true), "UTF-8"));
-            out.write(jsonMani.toJSONString().replace("\\",""));
-//          out.write("\n" + repo_path + "; " + artID + "; " + ft.format(date) + ";");
+            
+            ned_stark = ned_stark.replace("//","/");
+            
+            JSONObject jsonObj = new JSONObject();
+            
+            jsonObj.put("art_src_path", ned_stark);
+            jsonObj.put("art_ID", artID);
+            jsonObj.put("art_date", ft.format(date));
+            
+            // append json object in the manifest file
+            Writer  out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(mani_path, true), "UTF-8"));
+            out.write(jsonObj.toJSONString().replace("\\","")+"\n");
             out.close();
-        }   
+        }
         catch(Exception e) {
-            System.out.println(e);
-        }   
+            System.out.println(e+" - Manifest error");
+        } 
     }
-        public static int maniFileNo(String dirPath, String cmd) throws IOException{
+
+        //function to return the most recent version number of the manifest file for the given command
+    public static int maniFileNo(String dirPath, String cmd) throws IOException{
         int i = 1;
         List<String> al = new ArrayList<String>();
         String maniFile="mani_";
@@ -65,10 +71,7 @@ public class CreateManifest {
             if(maniCmd.contains(maniFile))
                 al.add(maniCmd);
         int lastFile = getLatestFilefromDir(al);
-//        String lastFile = getLatestFilefromDir(dirPath).getName();
-//        String[] parts = lastFile.split("_");
-//        i = Integer.parseInt(parts[2]);
-        
+
         return lastFile;
     }
 
@@ -80,7 +83,7 @@ public class CreateManifest {
             String fileName = file.toString();
             String parts[] = fileName.split("_");
             myArray[counter] = Integer.parseInt(parts[parts.length-2]);
-            System.out.println(myArray[counter]+"myArray --");
+//          System.out.println(myArray[counter]+"myArray --");
             counter ++;
         }
         Arrays.sort(myArray);        
