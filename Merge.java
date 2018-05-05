33ab481dc5e77cc364a9adf2cb6b732d0e19d14e
Rachayita Giri <rachayitagiri@gmail.com>
2018-05-04 22:07:14 -0700

    Calls the Run_Merge class to implement merge

--- Merge.java
+++ Merge.java
@@ -16,20 +16,43 @@ import java.io.IOException;
  *  sc: uses the singleton for input parsing
  * 
  */
+
 public class Merge implements Component_Input {
 
     private InputParser sc = InputParser.getParser();
     
+    /*
+	 * RunOperation: implemented so that the merge command can be carried out
+	 *		syntax: merge 'repo_name' 'version_name_1' 'version_name_2'
+     */
     @Override
     public void RunOperation() {
         
-        String folder1;
-        String folder2;
+        String versionR;
+        String versionT;
         
-        folder1 = sc.GetToken();
-        folder2 = sc.GetToken();
-                
-        CreateMerge.doMerge( folder1, folder2 );
+        //Read input version names 
+        versionR = sc.GetToken();
+        versionT = sc.GetToken();
+        
+        //Print version names
+        System.out.println( "Merge folders " + versionR + "and " + versionT);
+        System.out.println( "Merge to repo: " + repo_name); 
+
+        //Create array to hold command line arguments
+        arr = new String[ 4 ];
+        arr[0] = "merge";
+        arr[1] = repo_name;
+        arr[2] = versionR;
+        arr[3] = versionT;
+
+        //Call the merge function in Run_Merge.java
+        try{
+        	Run_Merge.doMerge( arr );
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+        
 
     }
 
